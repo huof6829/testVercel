e149bfc59523102bb3efc205f15329c71d25067b
huof6829 <68298506@qq.com>
2024-08-08 13:20:58 +0000

    test

--- api/hello.go
+++ api/hello.go
@@ -7,7 +7,9 @@ import (
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!\n")
-	fmt.Fprintf(w, "r.url:%+v, host:%+v\n", r.URL.Path, r.Host)
-	fmt.Fprintf(w, "r:%+v\n", r)
+	// fmt.Fprintf(w, "r.url:%+v, host:%+v\n", r.URL.Path, r.Host)
+	// fmt.Fprintf(w, "r:%+v\n", r)
+	fmt.Fprintf(w, "w:%+v\n", w)
+	fmt.Fprintf(w, "w.header:%+v\n", w.Header())
 
 }
